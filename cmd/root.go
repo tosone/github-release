ec1d3ae5a921653289998207ae677e9baf8dd38e
Tosone <i@tosiney.com>
2018-01-21 15:29:19 +0800

    Add something guide info for beginners.
    Signed-off-by: Tosone <i@tosiney.com>

--- cmd/root.go
+++ cmd/root.go
@@ -20,25 +20,25 @@ var dir string
 // RootCmd represents the base command when called without any sub commands
 var RootCmd = &cobra.Command{
 	Use:   common.AppName,
-	Short: "",
-	Long:  ``,
+	Short: "Release files and changelog to github release page.",
+	Long:  `Release files and changelog to github release page.`,
 }
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "get version",
-	Long:  ``,
+	Short: "Get version",
+	Long:  `The version that build detail information.`,
 	Run: func(_ *cobra.Command, _ []string) {
-		initConfig()
 		version.Initialize()
 	},
 }
 
 var createCmd = &cobra.Command{
 	Use:   "create",
-	Short: "",
-	Long:  ``,
-	Args:  cobra.MinimumNArgs(0),
+	Short: "Create a new release on github release page.",
+	Long: `Create a new release on github release page. 
+The config file is current directory .release file and something else in env about users key info.`,
+	Args: cobra.MinimumNArgs(0),
 	Run: func(_ *cobra.Command, args []string) {
 		initConfig()
 		create.Initialize(dir, args...)
@@ -47,8 +47,8 @@ var createCmd = &cobra.Command{
 
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "",
-	Long:  ``,
+	Short: "Delete a tag release from github release page.",
+	Long:  `Delete a tag release from github release page.`,
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(_ *cobra.Command, args []string) {
 		initConfig()
@@ -62,7 +62,7 @@ func init() {
 	if currPath, err = os.Getwd(); err != nil {
 		logging.Fatal(err)
 	}
-	createCmd.PersistentFlags().StringVarP(&dir, "dir", "d", currPath, "config file")
+	createCmd.PersistentFlags().StringVarP(&dir, "dir", "d", currPath, "execute path")
 
 	RootCmd.AddCommand(createCmd)
 	RootCmd.AddCommand(versionCmd)
